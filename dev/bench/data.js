b607840205a6bee00377745f828e0e23700d7641
github-action-benchmark <github@users.noreply.github.com>
2022-09-02 13:46:06 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b8eed710760ceecd06eeffdf9fcf7052a5da0c34

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662126120977,
+  "lastUpdate": 1662126366553,
   "repoUrl": "https://github.com/zuoxunwu/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -84,6 +84,51 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/test",
             "value": 6.858324688965878,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b8eed710760ceecd06eeffdf9fcf7052a5da0c34",
+          "message": "Merge pull request #203 from imelnyk1337/summer-programme\n\nAdded ability to change number of thread from command line",
+          "timestamp": "2022-08-26T17:51:14+02:00",
+          "tree_id": "dfb86f4703f2271533dcdc29661b5dad23b639f6",
+          "url": "https://github.com/zuoxunwu/FCCAnalyses/commit/b8eed710760ceecd06eeffdf9fcf7052a5da0c34"
+        },
+        "date": 1662126363632,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 15.72321367263794,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 22.1511332988739,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 16.741502285003662,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
