d6ae3a8a180e9133aa84487399c6dab4a144c462
github-action-benchmark <github@users.noreply.github.com>
2023-03-13 19:40:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7936a9960c50db086579e741f9ef76e0d525da3c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675174056414,
+  "lastUpdate": 1678736440030,
   "repoUrl": "https://github.com/zuoxunwu/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1176,6 +1176,51 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/test",
             "value": 5.712774948739597,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zuoxunwu@gmail.com",
+            "name": "Xunwu Zuo",
+            "username": "zuoxunwu"
+          },
+          "committer": {
+            "email": "zuoxunwu@gmail.com",
+            "name": "Xunwu Zuo",
+            "username": "zuoxunwu"
+          },
+          "distinct": true,
+          "id": "7936a9960c50db086579e741f9ef76e0d525da3c",
+          "message": "BuBc: Add a independent stage to study background composition",
+          "timestamp": "2023-03-13T20:30:55+01:00",
+          "tree_id": "df29787da845182aa622b3f13c7a14acf6294c2a",
+          "url": "https://github.com/zuoxunwu/FCCAnalyses/commit/7936a9960c50db086579e741f9ef76e0d525da3c"
+        },
+        "date": 1678736438471,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 13.350197076797485,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.677633285522461,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/test",
+            "value": 13.186206102371216,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
