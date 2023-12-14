d942f2a768455343aa356cb46dcfc94c11ab037f
github-action-benchmark <github@users.noreply.github.com>
2023-12-14 17:05:40 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 09d25efbb93456140a56ac1c5cf702672f329f44

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702573538955,
+  "lastUpdate": 1702573540266,
   "repoUrl": "https://github.com/zuoxunwu/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -3690,6 +3690,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09d25efbb93456140a56ac1c5cf702672f329f44",
+          "message": "Removing legacy style to run an analysis (#327)\n\n* Removing legacy style to run an analysis\r\n\r\n* Fix batch print\r\n\r\n* Batch mode for root\r\n\r\n* Remove runValidate",
+          "timestamp": "2023-12-14T10:51:24+01:00",
+          "tree_id": "5786ed48a5e30971c8950b507e75cacd897a4577",
+          "url": "https://github.com/zuoxunwu/FCCAnalyses/commit/09d25efbb93456140a56ac1c5cf702672f329f44"
+        },
+        "date": 1702573539861,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.953511472373885,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 10.419721368921978,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.760691200602885,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 7.035420202957329,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.870697780185237,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.2151673271731545,
+            "unit": "Evt/s"
           }
         ]
       }
